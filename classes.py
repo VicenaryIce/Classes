1645de4098ae1acd1ec190d041b56ddd72eeee57
VicenaryIce <sid.ramnathsing@gmail.com>
2024-09-04 18:01:50 +0200

    Add files via upload

---
+++ classes.py
@@ -0,0 +1,11 @@
+#classes function like libraries
+class Dogs:
+    def __init__(self,breed,size):
+        
+        self.breed = breed
+        self.size = size
+
+dog = Dogs('poodle','cosmic')
+hond = Dogs('bulldog','ant sized')
+print(hond.breed)
+
